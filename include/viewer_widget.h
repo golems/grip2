27119bc6421460472dadde4f9eb90c8724def2ff
Pete <pete.vieira@gmail.com>
2014-02-06 10:07:03 -0500

    put viewer_widget class into own files

--- include/viewer_widget.h
+++ include/viewer_widget.h
@@ -1,10 +1,30 @@
+// QT includes
+#include <QtCore/QTimer>
+#include <QtGui/QGridLayout>
+
+// OpenSceneGraph includes
+#include <osgViewer/CompositeViewer>
+#include <osgViewer/ViewerEventHandlers>
+#include <osgGA/OrbitManipulator>
+#include <osgDB/ReadFile>
+#include <osgQt/GraphicsWindowQt>
+#include <stdlib.h>
+#include <iostream>
 
 
+class ViewerWidget : public QWidget, public osgViewer::CompositeViewer
+{
+public:
+    ViewerWidget(osgViewer::ViewerBase::ThreadingModel threadingModel=osgViewer::CompositeViewer::SingleThreaded);
 
-//class ViewerWidget : public QWidget, public osgViewer::CompositeViewer
-//{
-// public:
-// ViewerWidget(osgViewer::ViewerBase::ThreadingModel threadingModel=osgViewer::CompositeViewer::SingleThreaded) : QWidget();
+    QWidget* addViewWidget(osg::Camera* camera, osg::Node* scene);
 
+    osg::Camera* createCamera(int x, int y, int w, int h, const std::string& name="", bool windowDecoration=false);
 
-//}
+    virtual void paintEvent( QPaintEvent* event )
+    { frame(); }
+
+protected:
+
+    QTimer _timer;
+};
