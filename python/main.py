6bf029400a1f253d0e18da643861c4cd915171f1
jkscholz <jonathan.scholz@gmail.com>
2014-08-13 13:25:41 -0700

    kinda got thread solution working in linux

--- python/main.py
+++ python/main.py
@@ -6,9 +6,10 @@ import threading
 if __name__ == '__main__':
 	pg = PyGrip()
 	print 'instantiated'
-	pg.create()
-	print 'created'
-	pg.loadScene("/Users/jscholz/vc/my_robot_models/scenes/krang_techway.urdf")
+	pg.run()
+	print 'running'
+	#pg.loadScene("/Users/jscholz/vc/my_robot_models/scenes/krang_techway.urdf")
+	pg.loadScene("/home/jscholz/vc/kore/etc/scenes/02-World-Collision.urdf")
 	print 'loaded'
 
 	import ipdb;ipdb.set_trace()
--- python/setup.py
+++ python/setup.py
@@ -2,16 +2,21 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-QT_HEADER_DIR = "/usr/local/include/"
+MAC=False
+if MAC:
+  QT_HEADER_DIR = "/usr/local/include/" # source install on OSX (jon)
+  EIGEN_INCLUDE_DIRS = ["/usr/local/include/eigen3"]
+  DART_INCLUDE_DIRS = ["/usr/local/include/dart/"]
+else:
+  QT_HEADER_DIR = "/usr/include/qt4/"
+  EIGEN_INCLUDE_DIRS = ["/usr/include/eigen3"]
+  DART_INCLUDE_DIRS = ["/usr/include/dart/"]
+
 QT_INCLUDE_DIRS = [QT_HEADER_DIR + dir for dir in ["QtDesigner","QtDeclarative","QtScriptTools","QtDBus","QtDesigner","QtXml",
 	"QtSql","QtOpenGL","QtNetwork","QtXmlPatterns","QtWebKit","QtGui","QtCore"]]
 
 GRIP_INCLUDE_PATH_PREFIX = "../"
 GRIP_INCLUDE_DIRS = ["../include", "../osgDart/", "../osgGolems/", "../qtWidgets/", "../build/qtWidgets"]
-
-EIGEN_INCLUDE_DIRS = ["/usr/local/include/eigen3"]
-
-DART_INCLUDE_DIRS = ["/usr/local/include/dart/"]
 
 INCLUDE_DIRS = [QT_HEADER_DIR] + QT_INCLUDE_DIRS + GRIP_INCLUDE_DIRS + EIGEN_INCLUDE_DIRS + DART_INCLUDE_DIRS
 
