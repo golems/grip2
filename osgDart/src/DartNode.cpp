d0c14dc01813ee4b8ef01b06d7781914f1618350
Ana Huaman <ahuaman3@gatech.edu>
2015-01-24 00:21:05 -0500

    Changed header calling ConstraintDynamics to ConstraintSolver

--- osgDart/src/DartNode.cpp
+++ osgDart/src/DartNode.cpp
@@ -47,7 +47,7 @@
 #include <dart/utils/sdf/SdfParser.h>
 #include <dart/collision/CollisionDetector.h>
 #include <dart/constraint/Constraint.h>
-#include <dart/constraint/ConstraintDynamics.h>
+#include <dart/constraint/ConstraintSolver.h>
 
 // osgDart includes
 #include "DartNode.h"
